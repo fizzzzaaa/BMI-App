72d9ddcc5103eec4f6f4779137dd085e57b83e3c
fizzzzaaa <fizaahmad015@gmail.com>
2024-11-05 09:33:02 +0500

    vid 04 created an input page

---
+++ lib/input_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'IconText.dart';
+import 'ContainerFile.dart';
+class InputPage extends StatefulWidget {
+
+  @override
+  InputPageState createState() => InputPageState();
+}
+class InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ), // AppBar
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: RepeatRefactorCode(
+                    colors:Color(0xFF1D1E33),
+                    cardwidget: RefactorTextandIcon(
+                      iconData:FontAwesomeIcons.person,
+                      label: "MALA",
+                    ),
+                  ),
+                ),
+                Expanded(
+                    child:RepeatRefactorCode(colors:Color(0xFF1D1E33),
+                      cardwidget: RefactorTextandIcon(
+                        iconData:FontAwesomeIcons.female,
+                        label: "Female",
+                      ),)
+
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child:RepeatRefactorCode(colors:Color(0xFF1D1E33),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child:RepeatRefactorCode(colors:Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: RepeatRefactorCode(colors:Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ); // End of Scaffold
+  }
+}
+
+
+
+
